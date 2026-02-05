cf543d8f5d8feca540e1e18e6a4afaa0697f71b7
Sre03 <103439536+Sre03@users.noreply.github.com>
2026-02-05 15:25:06 +0530

    Clean up dashboard markdown in app.py
    Removed unnecessary markdown header from the dashboard.

--- app.py
+++ app.py
@@ -98,7 +98,6 @@ df_movies, df_genres = load_data()
 # --- Dashboard Title ---
 st.title("🎬 MovieLens Cinema Explorer")
 st.markdown("### *Movie Space Navigator - https://sre03.github.io/MovieViz/ \n Find movies to watch based on the genre of your choice - an interactive visualizer*")
-st.markdown("### **")
 st.markdown("### *Discover patterns in movie releases, genres, and cinematic trends over time*")
 
 
